b9ce21b44bd78f3b83ac813b1fba49c7d97ce753
kareenasharma <kareenasharma2021@u.northwestern.edu>
2021-03-06 15:46:13 -0600

    finished timer

--- TerminaDora/Assets/Timer.cs
+++ TerminaDora/Assets/Timer.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class Timer : MonoBehaviour
 {
+    private static float time;
+    private static float minutes;
+    private static float seconds;
+    
+    Text timer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        time = 181;
+        timer = GetComponent<Text>();
+        timer.text = "3:00";
+        Debug.Log("3 minutes");
     }
 
     // Update is called once per frame
     void Update()
     {
+        time = time - Time.deltaTime;
+
+        minutes = Mathf.FloorToInt(time / 60);
+        seconds = Mathf.FloorToInt(time % 60);
+
+        minutes.ToString();
+        seconds.ToString();
+
+        if (time <= 1)
+        {
+            timer.text = "";
+            GameOverScript.endGame();
+        }
+        
+        if (seconds != 0)
+        {
+            timer.text = string.Concat(minutes, ":", seconds);
+        }
+        else
+        {
+            timer.text = string.Concat(minutes, ":00");
+        }
+        if (seconds < 10)
+        {
+            timer.text = string.Concat(minutes, ":0", seconds);
+        }
         
     }
 }
