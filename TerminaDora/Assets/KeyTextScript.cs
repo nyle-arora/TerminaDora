a92725dc5bf4cfc4510af2edc162a973ccf7c358
Nyle Arora <nylearora2021@u.northwestern.edu>
2021-03-06 13:34:30 -0600

    text is weird, but music

---
+++ TerminaDora/Assets/KeyTextScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class KeyTextScript : MonoBehaviour
+{
+
+	public static int keyCount = 0; 
+	Text kText; 
+	Vector3 doraPosn; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        kText = GetComponent<Text>();
+        doraPosn = GameObject.FindGameObjectsWithTag("Player")[0].transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//TRY #1
+        // Vector3 temp = transform.position; 
+        // temp.x = doraPosn.x + 7.4f; 
+        // temp.y = doraPosn.y + 5.5f;
+        // transform.position = temp; 
+        //Debug.Log(transform.position);
+
+    	//TRY #2
+        // transform.position = new Vector3(doraPosn.x + 7.4f, doraPosn.y + 5.5f, 0);
+
+        // Vector3 newPosn = new Vector3(doraPosn.x + 7.4f, doraPosn.y + 5.5f);
+        // Vector3 ourPosn = Camera.main.WorldToScreenPoint(newPosn);
+        // transform.position = ourPosn; 
+        kText.text = "howdy";
+    }
+}
